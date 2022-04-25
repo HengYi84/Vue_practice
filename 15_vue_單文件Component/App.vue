658741dd2046d56a8c4c12e3b45d20391b5213d9
hengyi84 <brother92407@marya.com.tw>
2022-04-25 22:11:52 +0800

    Add new chapter

---
+++ 15_vue_單文件Component/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <School></School>
+  </div>
+</template>
+
+<script>
+import School from "./School";
+
+export default {
+  name: App,
+  template: `
+    <APP></APP>
+  `,
+  component: {
+    School,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 15_vue_單文件Component/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>學校名稱:{{ schoolName }}</h2>
+    <h2>學校地址:{{ schoolAddress }}</h2>
+    <button @click="showMsg">點擊提示學校</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: School,
+  data() {
+    return { schoolName: "板新", schoolAddress: "新北市" };
+  },
+  methods: {
+    showMsg() {
+      console.log(this);
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+</style>
